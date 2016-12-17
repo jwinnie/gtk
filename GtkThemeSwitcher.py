56e1f3135a07aa44f0631b012460e93e3167763a
Francis Potter <fp@hgx.us>
2016-12-17 11:29:40 -0800

    Theme status and README

--- GtkThemeSwitcher.py
+++ GtkThemeSwitcher.py
@@ -1,25 +1,8 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import gi
 gi.require_version("Gtk","3.0")
 from gi.repository import Gtk as gtk
-import os
-
-class theme(gtk.Button):
-    name = "Theme"
-    description = "Description not available."
-    def draw(self):
-        self.main_container = gtk.Box(orientation=gtk.Orientation.VERTICAL)
-        self.add(self.main_container)
-        self.title_label = gtk.Label("<b>" + self.name + "</b>")
-        self.title_label.set_use_markup(True)
-        self.main_container.add(self.title_label)
-        self.add(self.main_container)
-        
-a = theme()
-a.name = "Hello"
-a.description = "World"
-a.draw()
-print(a)
+import os, subprocess
 
 # New window
 main_window = gtk.Window()
@@ -28,24 +11,43 @@ main_window.connect('delete-event', gtk.main_quit)
 main_window.set_border_width(10)
 main_window.set_title("GTK Theme Switcher")
 
-# List of themes container
+# Window container
 main_container = gtk.Box()
 main_container.set_orientation(gtk.Orientation.VERTICAL)
 main_container.set_spacing(10)
 
+# Themes container 
+themes_container = gtk.Box()
+themes_container.set_orientation(gtk.Orientation.VERTICAL)
+themes_container.set_spacing(5)
+scroll = gtk.ScrolledWindow(hexpand=True, vexpand=True)
+
+# Current theme
+def refresh():
+    current_theme = subprocess.Popen(['gsettings','get','org.gnome.desktop.interface','gtk-theme'], stdout=subprocess.PIPE).communicate()[0].decode('UTF-8')
+    current_theme_label.set_text("Current theme: " + current_theme)
+
+# Add themes to scrollbar
 def change_theme(button):
     print("User clicked theme " + button._theme_name)
-
-    return os.system("gsettings set org.gnome.desktop.interface gtk-theme '{}'".format(button._theme_name))
+    os.system("gsettings set org.gnome.desktop.interface gtk-theme '{}'".format(button._theme_name))
+    refresh()
 
 for theme_name in os.listdir("/usr/share/themes"):
     print("Found theme " + theme_name)
     theme_button = gtk.Button(label="{}".format(theme_name))
     theme_button._theme_name = theme_name
     theme_button.connect("clicked",change_theme)
-    main_container.add(theme_button)
+    themes_container.add(theme_button)
 
+scroll.add(themes_container)
+main_container.add(scroll)
+
+current_theme_label = gtk.Label("")
+main_container.add(current_theme_label)
+
+refresh()
 main_window.add(main_container)
-main_window.add(a)
+
 main_window.show_all()
 gtk.main()
--- README.md
+++ README.md
@@ -2,8 +2,14 @@
 GTK utilities
 
 ## Gtk Theme Switcher
-Simple theme switcher! Uses ```gsettings org.gnome.desktop.interface gtk-theme```.
+Simple theme switcher! Uses `gsettings org.gnome.desktop.interface gtk-theme`.
 
 ### Installation
+1. `git clone git://github.com/jwinnie/gtk.git`
+2. `gtk/install.sh`
+3. `rm -rf ./gtk`
+To uninstall:
+1. `sudo rm /usr/share/applications/us.stormdesign.jwinnie.gtkthemeswitcher.desktop`
+2. `sudo rm /usr/local/bin/GtkThemeSwitcher.py`
 
 
